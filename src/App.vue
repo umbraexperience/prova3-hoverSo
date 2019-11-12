202b19b26c321a774ed7244c365a6efd66310ca1
Santi <scros97@gmail.com>
2019-11-12 17:56:22 +0100

    WAT

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
     <div @mouseover="playSound">So 1</div>
     <p v-show="active">SONA</p> -->
 
-    <button @click="playSound">So CLICK</button>
+    <button @click="playSound">COMENÇA</button>
+    <button @click="soundLeft">LEFT</button>
+    <button @click="soundRight">RIGHT</button>
 
     <div>
       <input type="range" v-model="position" />
@@ -14,30 +16,51 @@
   </div>
 </template>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/howler/2.1.2/howler.spatial.min.js"></script>
-
 <script>
 // eslint-disable-next-line no-unused-vars
-import { Howl, Howler } from "howler";
+// import { Howl, Howler } from "howler";
+
+import Wad from "web-audio-daw";
 
 export default {
   name: "app",
   components: {},
   data: function() {
     return {
-      position: 0
+      position: 0,
+      bell: ""
     };
   },
   methods: {
-    playSound() {
+    /* playSound() {
       var sound = new Howl({
         src: ["http://www.largesound.com/ashborytour/sound/brobob.mp3"],
         autoplay: true
       });
 
-      var id1 = sound.play();
+      // sound.rate(2);
 
-      sound.rate(10, id1);
+      sound.volume(1, soundID);
+      sound.pos(-500, 0, 1000, soundID);
+
+      var soundID = sound.play();
+    }
+  } */
+    playSound() {
+      this.bell = new Wad({
+        source:
+          "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Oddio_Overplay/Christian_Bjoerklund/Skapmat/Christian_Bjoerklund_-_01_-_Hallon.mp3",
+        panning: 1
+      });
+      this.bell.play();
+
+      this.bell.stop();
+    },
+    soundLeft() {
+      this.bell.setPanning(-1, 2);
+    },
+    soundRight() {
+      this.bell.setPanning(1, 2);
     }
   }
 };
